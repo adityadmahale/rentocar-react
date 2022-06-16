0f285028c7a459c9324b581ebbd986aa1a4c90e7
Shaik Asaduddin <shaikasaduddin@TABD5.WPA.Dal.Ca>
2022-06-15 22:23:40 -0300

    feat(UI): Committed inventory managemet for adding cars and stations.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+// MUI imports
+import Box from '@mui/material/Box';
+
+// Component imports
+import NavBar from './components/nav-bar';
+import FabMenu from './components/fab-menu/fab-menu';
+
 function App() {
+
+  const getFabMenuPositionStyles = () => {
+    return {
+      position: 'absolute',
+      bottom: '20px',
+      right: '20px'
+    };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Box sx={getFabMenuPositionStyles()}>
+        <FabMenu></FabMenu>
+      </Box>
     </div>
   );
 }

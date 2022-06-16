de4e5cba5bff5a8c590c490e3a312e30e002309a
Gandhi Rajan Mahendran <gn479247@dal.ca>
2022-06-16 00:22:35 -0300

    modified folder structure, added routes, added user management feature files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import './App.css';
 
 // MUI imports
-import Box from '@mui/material/Box';
+
+// Prime React imports
+import "primereact/resources/themes/md-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+// React imports
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from 'react';
 
 // Component imports
-import NavBar from './components/nav-bar';
-import FabMenu from './components/fab-menu/fab-menu';
+import Success from './components/user-management/Success';
+import Registration from "./components/user-management/Registration";
+import Login from "./components/user-management/Login";
+import UserList from './components/user-management/UserList';
+import UserProfile from './components/user-management/UserProfile';
+import TicketsHome from './components/customer-support/TicketsHome';
+import InventoryHome from './components/inventory-management/InventoryHome';
 
 function App() {
-
-  const getFabMenuPositionStyles = () => {
-    return {
-      position: 'absolute',
-      bottom: '20px',
-      right: '20px'
-    };
-  };
+  const [id, setId] = useState([]);
+  // const getFabMenuPositionStyles = () => {
+  //   return {
+  //     position: 'absolute',
+  //     bottom: '20px',
+  //     right: '20px'
+  //   };
+  // };
 
   return (
     <div className="App">
-      <NavBar />
-      <Box sx={getFabMenuPositionStyles()}>
-        <FabMenu></FabMenu>
-      </Box>
+      < Router >
+        <Routes>
+          {/* Routes for User Management and Support Features */}
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/userlist" element={<UserList setId={setId} />} />
+          <Route path="/success" element={<Success />} />
+          <Route path="/userprofile" element={<UserProfile id={id} />} />
+          <Route path="/ticketshome" element={<TicketsHome />} />
+          {/* Routes for Inventory Management */}
+          <Route path="/inventoryhome" element={<InventoryHome />} />
+        </Routes>
+      </Router >
     </div>
   );
 }

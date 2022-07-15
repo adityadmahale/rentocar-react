753b3056db5dd864c281a4755933812a47154630
mmandaliya <mn638205@dal.ca>
2022-07-15 20:04:10 -0300

    view reservation implemented

---
+++ src/services/reservationService.js
@@ -0,0 +1,11 @@
+import http from "./httpService";
+
+const reservationsAPIEndpoint = "/reservations/";
+
+export const getReservations = () => {
+  return http.get(reservationsAPIEndpoint);
+};
+
+export const getSpecificReservations = (username) => {
+  return http.post(reservationsAPIEndpoint + "search", username)
+};

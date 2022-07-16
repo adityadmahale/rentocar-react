bcf4440c1324c914a40a08015548d3fb6ef60db9
Shaik Asaduddin <shaikasaduddin@TABD5.WPA.Dal.Ca>
2022-07-15 23:15:34 -0300

    httpurl(ui): added react app url

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 // Interceptor for handling errors
 axios.interceptors.response.use(null, (error) => {
